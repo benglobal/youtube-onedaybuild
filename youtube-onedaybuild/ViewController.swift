829dafefd7d1efba7ebd3cca4955d29ed855efd8
Benji Benjamin <benjibenjamin@MacBook-Pro.local>
2020-07-04 15:52:23 -0500

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Benji Benjamin on 7/4/20.
+//  Copyright © 2020 benglobal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
